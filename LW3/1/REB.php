04a44be0424f5491d1fda0713e434a85f07a74e2
TPAHCJIuT <100317595+TPAHCJIuT@users.noreply.github.com>
2022-03-10 08:57:06 +0300

    Update REB.php

--- LW3/1/REB.php
+++ LW3/1/REB.php
@@ -1,7 +1,8 @@
 <?php
-    $text = trim( $_GET['text'] );
-    while ( strpos($text,'  ') != false )
-    {
-        $text = str_replace( '  ',' ',$text );
-    }; 
-    echo $text;
+header("Content-Type: text/plain");
+$text = trim($_GET['text']);
+while (!(strpos($text, '  ')))
+{
+  $text = str_replace('  ', ' ', $text);
+}; 
+echo $text;
