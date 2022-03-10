a4cc2dc9a976b5b706232bc7355ae2565a4c101e
TPAHCJIuT <100317595+TPAHCJIuT@users.noreply.github.com>
2022-03-10 09:00:57 +0300

    Update REB.php

--- LW3/1/REB.php
+++ LW3/1/REB.php
@@ -1,9 +1,8 @@
 <?php
 header("Content-Type: text/plain");
-echo 'Введите текст'
 $text = trim($_GET['text']);
 while (!(strpos($text, '  ')))
 {
   $text = str_replace('  ', ' ', $text);
 }; 
-echo $text;
+echo 'text:', $text;
